e41c9cb8d3b547058844b1e876c1929257c9d5d8
Posijour <168458508+Posijour@users.noreply.github.com>
2026-02-19 17:48:04 +0200

    Add files via upload

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ import os
 SUPABASE_URL = "https://qcusrlmueapuqbjwuwvh.supabase.co"
 SUPABASE_KEY = "sb_publishable_VsMaZGz98nm5lSQZJ-g-kQ_bUOfSO_r"
 
-DEFAULT_WINDOW_HOURS = 6
+DEFAULT_WINDOW_HOURS = 12
