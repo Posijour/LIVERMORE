deb525736a4947ef642d39fdd70af857627e5c9b
Posijour <168458508+Posijour@users.noreply.github.com>
2026-02-20 14:50:35 +0200

    Update config.py

--- config.py
+++ config.py
@@ -4,3 +4,9 @@ SUPABASE_URL = "https://qcusrlmueapuqbjwuwvh.supabase.co"
 SUPABASE_KEY = "sb_publishable_VsMaZGz98nm5lSQZJ-g-kQ_bUOfSO_r"
 
 DEFAULT_WINDOW_HOURS = 12
+
+DATA_SCOPE = {
+    "futures": "multi-symbol",
+    "options": "BTC / ETH",
+    "vol": "BTC / ETH",
+}
