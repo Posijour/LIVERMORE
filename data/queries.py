e624e6e3f4f9dd1ceab973ac741e26f3aceed26f
Posijour <168458508+Posijour@users.noreply.github.com>
2026-02-28 16:54:07 +0200

    Update queries.py

--- data/queries.py
+++ data/queries.py
@@ -9,7 +9,7 @@ def load_okx_market_state(ts_from, ts_to):
     return client.fetch("okx_market_state", ts_from, ts_to, symbol="MARKET")
 
 def load_bybit_market_state(ts_from, ts_to):
-    return client.fetch("bybit_market_state", ts_from, ts_to, symbol="MARKET")
+    return client.fetch("bybit_market_state", ts_from, ts_to, symbol=None)
 
 def load_deribit(ts_from, ts_to, symbol=None):
     return client.fetch("deribit_vbi_snapshot", ts_from, ts_to, symbol=symbol)
